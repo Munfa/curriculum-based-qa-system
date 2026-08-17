fbac1ce0bf9a8ad8757c112b5da4fda5f0bc90f1
Munfa <munfa2014@gmail.com>
2026-08-17 22:51:18 +0600

    Update files

--- generation_grading/llm.py
+++ generation_grading/llm.py
@@ -1,17 +1,23 @@
 import os
 import json
+from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+load_dotenv()
+
 MODEL = "gemini-2.5-flash-lite"
 
-client = genai.Client(
-    api_key=os.environ["gemini_api_key"]
-)
+api_key = genai.Client(api_key=os.environ["gemini_api_key"])
+
+if not api_key:
+    print("Error: API Key not found!")
+else:
+    print("API Key loaded successfully.")
 
 def generate(prompt: str, response_schema: dict) -> dict:
 
-    response = client.models.generate_content(
+    response = api_key.models.generate_content(
         model=MODEL,
         contents=prompt,
         config=types.GenerateContentConfig(
